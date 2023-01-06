0d03f36cfe8f358b29e40a17046b40ceca91c129
im.anggasaputra <im.anggasaputra@gmail.com>
2023-01-06 17:59:13 +0700

    add migration thread comments table

---
+++ migrations/1673002104820_craete-table-thread-comments.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('thread_comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primary: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNUll: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNUll: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNUll: true,
+    },
+  });
+
+  pgm.addConstraint('thread_comments', 'fk_thread_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+  pgm.addConstraint('thread_comments', 'fk_thread_comments.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('thread_comments', 'fk_thread_comments.thread_id_threads.id');
+  pgm.dropConstraint('thread_comments', 'fk_thread_comments.user_id_users.id');
+  pgm.dropTable('thread_comments');
+};
